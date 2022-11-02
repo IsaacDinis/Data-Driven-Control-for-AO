9344b23224a30fce6088fb956cdb0daccc0c6d9f
Isaac <isaac@desklin03.obs.unige.ch>
2022-11-02 16:59:31 +0100

    ntd

---
+++ empirical_KL/wao.py
@@ -0,0 +1,14 @@
+n_act_square = wao.config.p_dms[0].get_nact()
+v = np.zeros(n_act_square**2)
+
+wao.supervisor.rtc.set_perturbation_voltage(0, "", v)
+wao.supervisor.next()
+wao.supervisor.next()
+slopes = wao.supervisor.rtc.get_slopes(0)
+dummy = S2A @ slopes
+
+supervisor.rtc.set_perturbation_voltage(0, "", hadamard_matrix[:n_act_square,1]*ampli)
+
+inf_mat, _ = wao.supervisor.basis.compute_modes_to_volts_basis("KL2V") # or "KL2V" [nvolts ,nmodes]
+
+wao.supervisor.rtc.set_perturbation_voltage(0, "", inf_mat[:,0])
