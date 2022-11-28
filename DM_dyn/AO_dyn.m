38634a50fe253960cddab27e220bcfbbc315e8ba
Isaac Dinis <isaac.dinis@epfl.ch>
2022-11-28 20:17:02 +0100

    ntd

--- DM_dyn/AO_dyn.m
+++ DM_dyn/AO_dyn.m
@@ -11,17 +11,22 @@ K_d = g/(1-z^-1);
 RTC_c = d2c(K_d,'zoh')*exp(-s*Td);
 K_c = d2c(K_d,'zoh'); 
 K2_c = g/(1-exp(-s*Ts));
-ZOH_c = (1-exp(-s*Ts))/(s*Ts);
+ZOH_c = (1-exp(-s*Ts))/(Ts*s);
 %%
+T = 0.02;
+fs_sim = 4*fs; 
+t = 0:1/fs_sim:T-1/fs_sim;
+
 u = ones(1,length(t));
-T = 1;
-t = 0:1/fs:T-1/fs;
 
-
-S1 = feedback(1,K_c*ZOH_c);
+S1 = feedback(1,K_d);
 S2 = feedback(1,K2_c);
-S3 = feedback(1,K_d);
+S3 = feedback(1,K2_c*ZOH_c);
+
 figure()
 step(S1,S2,S3);
-figure()
-lsim(S1,u,t)
+legend()
+% 
+% figure()
+% lsim(S1,u,t)
+https://eng.libretexts.org/Bookshelves/Industrial_and_Systems_Engineering/Book%3A_Introduction_to_Control_Systems_(Iqbal)/07%3A_Design_of_Sampled-Data_Systems/7.02%3A_Pulse_Transfer_Function
