c7dd3d7a1a2afdb46a99b1e52e64ed690322c549
Isaac <isaac@desklin03.obs.unige.ch>
2022-11-29 17:05:27 +0100

    ntd

--- DM_dyn/AO_dyn.m
+++ DM_dyn/AO_dyn.m
@@ -12,6 +12,11 @@ RTC_c = d2c(K_d,'zoh')*exp(-s*Td);
 K_c = d2c(K_d,'zoh'); 
 K2_c = g/(1-exp(-s*Ts));
 ZOH_c = (1-exp(-s*Ts))/(Ts*s);
+
+w_DM = 1200;
+s_dm = 0.1;
+DM_c = w_DM.^2/(s^2+2*s_dm*w_DM*s+w_DM.^2);
+DM_d = c2d(DM_c,Ts,'tustin');
 %%
 T = 0.02;
 fs_sim = 4*fs; 
@@ -19,14 +24,22 @@ t = 0:1/fs_sim:T-1/fs_sim;
 
 u = ones(1,length(t));
 
-S1 = feedback(1,K_d);
-S2 = feedback(1,K2_c);
-S3 = feedback(1,K2_c*ZOH_c);
+S1 = feedback(1,K_c);
+S2 = feedback(1,K_c*DM_c);
+S3 = feedback(1,K_d*DM_d);
+
+% figure()
+% step(S1,S2,S3);
+% legend()
 
 figure()
-step(S1,S2,S3);
+bodemag(S1,S2,S3)
+
+figure()
+bodemag(DM_c)
 legend()
+
 % 
 % figure()
 % lsim(S1,u,t)
-https://eng.libretexts.org/Bookshelves/Industrial_and_Systems_Engineering/Book%3A_Introduction_to_Control_Systems_(Iqbal)/07%3A_Design_of_Sampled-Data_Systems/7.02%3A_Pulse_Transfer_Function
+% https://eng.libretexts.org/Bookshelves/Industrial_and_Systems_Engineering/Book%3A_Introduction_to_Control_Systems_(Iqbal)/07%3A_Design_of_Sampled-Data_Systems/7.02%3A_Pulse_Transfer_Function
---
+++ DM_dyn/ao_dd_controller.m
@@ -0,0 +1,99 @@
+function Kdd =  ao_dd_controller(fs,w,order,W1,W3,W32,solver)
+    %% SYS model
+    Ts = 1/fs;
+    
+    delay = tf([1],[1,0,0],Ts); % 2 samples delay
+
+    w_DM = 1200;
+    s_dm = 0.1;
+    DM_c = tf(w_DM.^2,[1,2*s_dm*w_DM,w_DM.^2]);
+    DM_d = c2d(DM_c,Ts,'tustin');
+
+    w_logspace = utils.logspace2(w(2),w(end),500);
+    DM_f = abs(frd(DM_c,w_logspace,'rad/s'));
+    DM_f = squeeze(DM_f.ResponseData);
+    G = frd(DM_c,w_logspace,'rad/s');
+    %% Initial controller
+    g = 0.2;
+    K0 = tf([g,0],[1,-1],Ts);
+%     S0 = feedback(1,G*K0);
+%     disp(['Eigenvalues closed-loop using initial controller: ', ...
+%         num2str(max(abs(eig(S0)))), ' (stable CL)']) 
+%                                                   % < 1 --> Stable Closed-loop
+    
+    %% Controller order
+    Fy = [1,-1]; % fixed parts in denominator (integrator).
+    Fx = 1;
+    [num, den] = tfdata(K0, 'v');
+    den(order + 1) = 0; % zero padding
+    num(order + 1) = 0; % zero padding
+    num_new = deconv(num, Fx);
+    den_new = deconv(den, Fy);
+    
+    %% SET-UP system info
+    
+    [SYS, OBJ, CON, PAR] = utils.emptyStruct(); % load empty structure
+    
+    ctrl = struct('num',num_new,'den',den_new,'Ts',Ts,'Fx',Fx,'Fy',Fy); % assemble controller
+    
+    SYS.controller = ctrl;
+    SYS.model = G; % Specify model(s)
+    
+    
+%     w_logspace = linspace(w(2),w(end),500);
+    SYS.W = w_logspace;  
+    
+    %% Objectives
+
+    OBJ.o2.W1 = W1;
+%     OBJ.oinf.W2 = W3;
+%     OBJ.o2.W3 = W32;
+%     CON.W3 = tf(0.99,1,1/fs);
+    %% Solve problem
+    PAR.tol = 1e-4;
+    PAR.maxIter = inf;
+    % PAR.radius = 0.99;
+%     tic
+    [controller,obj] = datadriven(SYS,OBJ,CON,PAR,solver);
+%     toc
+    
+    
+    %% Analysis using optimal controller
+    
+    Kdd = utils.toTF(controller); % 
+%     disp(['Eigenvalues close-loop using initial controller: ', ...
+%         num2str(max(abs(eig(feedback(1,G*Kdd))))), ' (stable CL)']) 
+%                                                   % < 1 --> Stable Closed-loop
+    
+    %% Datadriven
+%     U_dd = feedback(Kdd,G);
+%     U_int = feedback(K0,G);
+%     S_dd = feedback(1,G*Kdd);
+%     S_int = feedback(1,G*K0);
+%     opt = stepDataOptions('StepAmplitude',1);
+%     
+%     figure()
+%     
+%     subplot(2,2,1)
+%     step(S_dd,S_int,opt)
+%     title('Step Response');
+%     legend('Datadriven', 'Integrator');
+% 
+%     subplot(2,2,2)
+%     step(U_dd,U_int,opt)
+%     title('Control signal');
+%     legend('Datadriven', 'Integrator');
+% 
+%     subplot(2,2,3)
+%     bodemag(U_dd,U_int,{w(2),w(end)});
+%     title('Sensitivity function U');
+%     legend('Datadriven', 'Integrator');
+% 
+%     subplot(2,2,4)
+%     bodemag(S_dd,S_int,W1^-1,{w(2),w(end)})
+%     legend('Datadriven', 'Integrator','W1^{-1}');
+%     title('Sensitivity function S');
+%     
+%     sgtitle('Datadriven controller') 
+
+end
