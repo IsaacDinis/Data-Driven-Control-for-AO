aa2c1390eef6fbd995221805e42ba1e63c962d52
Isaac Dinis <isaac.dinis@epfl.ch>
2023-04-26 13:21:20 +0200

    ntd

--- DM_dyn/matlab/create_timeseries.m
+++ DM_dyn/matlab/create_timeseries.m
@@ -1,9 +1,9 @@
-dist_matrix_path = '../data/single_mode_dist_ProxCen_1_4000.mat';
+dist_matrix_path = '../data/single_mode_dist_nonoise_1_4000.mat';
 dist = load(dist_matrix_path).data';
 dist = double(dist(:));
 fs = 2000;
 t = 0:1/fs:size(dist,1)/fs-1/fs;
 plop = timeseries(dist, t);
-save('../simulink/single_mode_dist_ProxCen_1_4000_ts.mat','plop','-v7.3')
+save('../simulink/single_mode_dist_nonoise_1_4000_ts.mat','plop','-v7.3')
 
 % save 'dist.mat' -v7.3 timeseries_object
