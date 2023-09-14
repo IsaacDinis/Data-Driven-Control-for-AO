b362d773855013bdb23e36b55137a76f59796410
Isaac Dinis <isaac.dinis@epfl.ch>
2023-09-14 22:04:22 +0200

    ntd

--- ristretto/compass/phase_plot.py
+++ ristretto/compass/phase_plot.py
@@ -2,12 +2,18 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 class phase_plot:
-    def __init__(self, title):
+    def __init__(self, title, refresh_rate):
         self.fig, self.ax = plt.subplots(constrained_layout=True)
         self.fig.suptitle(title)
+        self.refresh_rate = refresh_rate
+        self.count = 0
 
     def plot(self, phase, subtitle = ""):
-        self.ax.imshow(phase)
-        self.ax.set_title(subtitle)
-        self.fig.canvas.draw()
-        self.fig.canvas.flush_events()
+        if self.count == 0:
+            self.ax.imshow(phase)
+            self.ax.set_title(subtitle)
+            self.fig.canvas.draw()
+            self.fig.canvas.flush_events()
+        self.count += 1
+        if self.count == self.refresh_rate:
+            self.count = 0
